7dd50981f4d1d8ed9199ed5e4158caecd797c2d1
PrevinBogopa <previnbogopa@icloud.com>
2023-09-15 11:25:35 +0200

    Update script.js

--- guess number game/script.js
+++ guess number game/script.js
@@ -1,3 +1,16 @@
 'use strict';
-document.querySelector('.message').textContent='Correct Number';
-console.group(document.querySelector.querySelector('.message').textContent);
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent='Correct Number';
+// document.querySelector('.number').textContent=13;
+// document.querySelector('.score').textContent=10;
+
+// console.log((document.querySelector('.guess').value));
+// document.querySelector('.guess').value=23;
+
+document.querySelector('.check').addEventListener('click',function(){
+    console.log(document.querySelector('.guess').value);
+
+    
+})
+
