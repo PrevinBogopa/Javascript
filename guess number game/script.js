d76ca406f304da9594300fd83faa2530a9226536
PrevinBogopa <previnbogopa@icloud.com>
2023-09-15 14:32:07 +0200

    Update script.js

--- guess number game/script.js
+++ guess number game/script.js
@@ -8,14 +8,62 @@
 // console.log((document.querySelector('.guess').value));
 // document.querySelector('.guess').value=23;
 
-const number=Math.trunc(Math.random()*21)
-document.querySelector('.number').textContent=number;
+let score=20;
+let secretnumber=Math.trunc(Math.random()*21)
+
 document.querySelector('.check').addEventListener('click',function(){
    const guess=Number(document.querySelector('.guess').value);
    console.log(guess,typeof guess);
 if(!guess){
     document.querySelector('.message').textContent='No number';
 }
-    
+else if(guess===secretnumber){
+    document.querySelector('.message').textContent='Correct Number';
+    document.querySelector('body').style.backgroundColor='#60b347';
+    document.querySelector('.number').style.width='30rem';
+    document.querySelector('.number').textContent=secretnumber;
+}
+else if(guess>secretnumber){
+
+        
+if(score>0){
+    document.querySelector('.message').textContent='Too high,go lower';
+    score--;
+    document.querySelector('.score').textContent=score;
+}else{
+    document.querySelector('.message').textContent='You Lost the game!';
+    document.querySelector('.score').textContent=0; 
+    document.querySelector('body').style.backgroundColor='#60b347';
+
+}
+  
+}
+else if(guess<secretnumber){
+   
+    if(score>1){
+        document.querySelector('.message').textContent='Too Low,go higher';
+        score--;
+        document.querySelector('.score').textContent=score;
+    }else{
+        document.querySelector('.message').textContent='You Lost the game!';
+        document.querySelector('.score').textContent=0; 
+    }
+}
+
 });
 
+document.querySelector('.again').addEventListener('click',function(){
+
+     score=20;
+     secretnumber=Math.trunc(Math.random()*21);
+     document.querySelector('.message').textContent='Start guessing...';
+     document.querySelector('.score').textContent=score;
+    
+     document.querySelector('.number').textContent='?';
+     document.querySelector('.guess').value='';
+    
+
+     document.querySelector('body').style.backgroundColor='#222222';
+     document.querySelector('.number').style.width='15rem';
+///front schock n back....shock mountain *2 
+})
